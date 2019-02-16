ec9d27b90a70824d1146d39913be96986f6964b4
James <igknightion@gmail.com>
2019-02-16 12:09:46 -0600

    BH - commented limelight code

--- JV Robot 2019/src/main/java/frc/robot/Robot.java
+++ JV Robot 2019/src/main/java/frc/robot/Robot.java
@@ -24,8 +24,6 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.cscore.UsbCamera;
 
-
-
 /**
  * The VM is configured to automatically run this class, and to call the
  * functions corresponding to each mode, as described in the IterativeRobot
@@ -38,41 +36,41 @@ public class Robot extends IterativeRobot {
   private static final String kCustomAuto = "My Auto";
   private String m_autoSelected;
   private final SendableChooser<String> m_chooser = new SendableChooser<>();
-    double x;
-    double y;
-    // Doubles for driving axies
-    UsbCamera Cam0;
-    UsbCamera Cam1;
-    Joystick joy = new Joystick(0);
-    Talon frontRight = new Talon(0);
-    Talon frontLeft = new Talon(3);
-    Talon backRight = new Talon(1);
-    Talon backLeft = new Talon(2);
-    // Names driving motor controllers (Correct PDM ports)
-    Relay claw = new Relay(0);
-    // Names claw motor controller? hopefully
-
-    /** Talon forback = new Talon(5);
-    // Names claw moving back and forth motor controller (No idea if right PDM port)
-    Talon lift1 = new Talon(6);                                                                                         Not using lift code or forward and back
-    Talon lift2 = new Talon(7);
-    // Names lift motor controller (No idea if right PDM port and if actually using lift)
-    */
-    SpeedControllerGroup Left = new SpeedControllerGroup(frontLeft, backLeft);
-    SpeedControllerGroup Right = new SpeedControllerGroup(frontRight, backRight);
-    //SpeedControllerGroup lift = new SpeedControllerGroup(lift1, lift2);
-    //Combines two motor controllers
-    DifferentialDrive fullSendDrive = new DifferentialDrive(Left, Right);
-    //Combines left and right into one
-    NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
-    NetworkTableEntry tx = table.getEntry("tx");
-    NetworkTableEntry ty = table.getEntry("ty");
-    NetworkTableEntry ta = table.getEntry("ta");
-    //Bunch of limelight stuff that hopefully works
+  double x;
+  double y;
+  // Doubles for driving axies
+  UsbCamera Cam0;
+  UsbCamera Cam1;
+  Joystick joy = new Joystick(0);
+  Talon frontRight = new Talon(0);
+  Talon frontLeft = new Talon(3);
+  Talon backRight = new Talon(1);
+  Talon backLeft = new Talon(2);
+  // Names driving motor controllers (Correct PDM ports)
+  Relay claw = new Relay(0);
+  // Names claw motor controller? hopefully
 
   /**
-   * This function is run when the robot is first started up and should be
-   * used for any initialization code.
+   * Talon forback = new Talon(5); // Names claw moving back and forth motor
+   * controller (No idea if right PDM port) Talon lift1 = new Talon(6); Not using
+   * lift code or forward and back Talon lift2 = new Talon(7); // Names lift motor
+   * controller (No idea if right PDM port and if actually using lift)
+   */
+  SpeedControllerGroup Left = new SpeedControllerGroup(frontLeft, backLeft);
+  SpeedControllerGroup Right = new SpeedControllerGroup(frontRight, backRight);
+  // SpeedControllerGroup lift = new SpeedControllerGroup(lift1, lift2);
+  // Combines two motor controllers
+  DifferentialDrive fullSendDrive = new DifferentialDrive(Left, Right);
+  // Combines left and right into one
+  // NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+  // NetworkTableEntry tx = table.getEntry("tx");
+  // NetworkTableEntry ty = table.getEntry("ty");
+  // NetworkTableEntry ta = table.getEntry("ta");
+  // Bunch of limelight stuff that hopefully works
+
+  /**
+   * This function is run when the robot is first started up and should be used
+   * for any initialization code.
    */
   @Override
   public void robotInit() {
@@ -85,12 +83,13 @@ public class Robot extends IterativeRobot {
   }
 
   /**
-   * This function is called every robot packet, no matter the mode. Use
-   * this for items like diagnostics that you want ran during disabled,
-   * autonomous, teleoperated and test.
+   * This function is called every robot packet, no matter the mode. Use this for
+   * items like diagnostics that you want ran during disabled, autonomous,
+   * teleoperated and test.
    *
-   * <p>This runs after the mode specific periodic functions, but before
-   * LiveWindow and SmartDashboard integrated updating.
+   * <p>
+   * This runs after the mode specific periodic functions, but before LiveWindow
+   * and SmartDashboard integrated updating.
    */
   @Override
   public void robotPeriodic() {
@@ -99,68 +98,48 @@ public class Robot extends IterativeRobot {
     // Sets x and y to Axis values
     fullSendDrive.arcadeDrive(-x, y);
     // Motors controllers get x and y values so it can full send it my dude
-    double x = tx.getDouble(0.0);
-    double y = ty.getDouble(0.0);
-    double area = ta.getDouble(0.0);
-    //read values periodically
-    SmartDashboard.putNumber("LimelightX", x);
-    SmartDashboard.putNumber("LimelightY", y);
-    SmartDashboard.putNumber("LimelightArea", area);
-    //post to smart dashboard periodically
-    NetworkTableInstance.getDefault().getTable("limelight").getEntry("<variablename>").getDouble(0);
+    // double xlight = tx.getDouble(0.0);
+    // double ylight = ty.getDouble(0.0);
+    // double area = ta.getDouble(0.0);
+    // read values periodically
+    // SmartDashboard.putNumber("LimelightX", xlight);
+    // SmartDashboard.putNumber("LimelightY", ylight);
+    // SmartDashboard.putNumber("LimelightArea", area);
+    // post to smart dashboard periodically
+    // NetworkTableInstance.getDefault().getTable("limelight").getEntry("<variablename>").getDouble(0);
     if (joy.getRawButton(1) == true) {
       claw.set(Value.kForward);
-      //Claw grabs hatch (Trigger)
-    } 
-    else if (joy.getRawButton(2) == true) {
+      // Claw grabs hatch (Trigger)
+    } else if (joy.getRawButton(2) == true) {
       claw.set(Value.kReverse);
-      //Claw lets go of hatch
-    }
-    else {
+      // Claw lets go of hatch
+    } else {
       claw.set(Relay.Value.kOff);
     }
 
-    
-    /**                                                                            Not using forward and backward and lift code
-      if (joy.getRawButton(5)) {
-      forback.set(0.2);
-      //moves claw forward (Top left)
-    }
-    else if (joy.getRawButton(3)) {
-      forback.set(-0.2);
-      //moves claw backward (Bottom Left)
-    }
-    else {
-      forback.set(0);
-      //claw doesn't move naturally
-    }
-
-    if (joy.getRawButton(6)) {
-      lift.set(0.2);
-    //lift moves up (Top Right)
-    }
-    else if (joy.getRawButton(4)) {
-      lift.set(-0.2);
-      //lift moves down (Bottom Right)
-    }
-    else {
-      lift.set(0);
-      //lift doesn't move naturally
-      }
-      */
-    }
-  
+    /**
+     * Not using forward and backward and lift code if (joy.getRawButton(5)) {
+     * forback.set(0.2); //moves claw forward (Top left) } else if
+     * (joy.getRawButton(3)) { forback.set(-0.2); //moves claw backward (Bottom
+     * Left) } else { forback.set(0); //claw doesn't move naturally }
+     * 
+     * if (joy.getRawButton(6)) { lift.set(0.2); //lift moves up (Top Right) } else
+     * if (joy.getRawButton(4)) { lift.set(-0.2); //lift moves down (Bottom Right) }
+     * else { lift.set(0); //lift doesn't move naturally }
+     */
+  }
 
   /**
    * This autonomous (along with the chooser code above) shows how to select
-   * between different autonomous modes using the dashboard. The sendable
-   * chooser code works with the Java SmartDashboard. If you prefer the
-   * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-   * getString line to get the auto name from the text box below the Gyro
+   * between different autonomous modes using the dashboard. The sendable chooser
+   * code works with the Java SmartDashboard. If you prefer the LabVIEW Dashboard,
+   * remove all of the chooser code and uncomment the getString line to get the
+   * auto name from the text box below the Gyro
    *
-   * <p>You can add additional auto modes by adding additional comparisons to
-   * the switch structure below with additional strings. If using the
-   * SendableChooser make sure to add them to the chooser code above as well.
+   * <p>
+   * You can add additional auto modes by adding additional comparisons to the
+   * switch structure below with additional strings. If using the SendableChooser
+   * make sure to add them to the chooser code above as well.
    */
   @Override
   public void autonomousInit() {
@@ -176,13 +155,13 @@ public class Robot extends IterativeRobot {
   @Override
   public void autonomousPeriodic() {
     switch (m_autoSelected) {
-      case kCustomAuto:
-        // Put custom auto code here
-        break;
-      case kDefaultAuto:
-      default:
-        // Put default auto code here
-        break;
+    case kCustomAuto:
+      // Put custom auto code here
+      break;
+    case kDefaultAuto:
+    default:
+      // Put default auto code here
+      break;
     }
   }
 
